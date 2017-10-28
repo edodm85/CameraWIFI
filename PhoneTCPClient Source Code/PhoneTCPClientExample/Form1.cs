eedd665e60562528d7b0918cb7d658ef33cad7fa
Edoardo <edoardodemarchi@gmail.com>
2017-10-28 21:32:30 +0200

    Privacy and Source Code

---
+++ PhoneTCPClient Source Code/PhoneTCPClientExample/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneTCPClientExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Create a TCP/IP  socket.
+        TcpClient oTCPClient;
+        NetworkStream stream;
+        Thread receiveThread;
+        Byte[] dataReceive;
+        Int32 bytesread;
+        String stringData;
+
+
+        /// <summary>
+        /// Connect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonTCPConnect_Click(object sender, EventArgs e)
+        {
+            String sIPAddress = textBoxIpAddress.Text;
+            String sPort = textBoxPort.Text;
+            if (sIPAddress != "" && sPort != "")
+            {
+                try
+                {
+                    IPAddress ipAd = IPAddress.Parse(sIPAddress);
+                    oTCPClient = new TcpClient();
+                    
+                    // Connect
+                    oTCPClient.Connect(ipAd, int.Parse(sPort));
+                    labelTCPStatus.Text = "Status: Connected";
+                    stream = oTCPClient.GetStream();
+
+
+                    // Thread
+                    receiveThread = new Thread(new ThreadStart(ListenForInMex));
+                    receiveThread.Priority = ThreadPriority.Normal;
+                    receiveThread.Start();
+                }
+                catch(Exception ee)
+                { }
+            }
+        }
+
+
+        /// <summary>
+        /// Disconnect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonTCPDisconnect_Click(object sender, EventArgs e)
+        {
+            if(oTCPClient != null)
+            {
+                receiveThread.Abort();
+
+                // Disconnect
+                oTCPClient.Close();
+                labelTCPStatus.Text = "Status: Disconnected";
+            }
+        }
+
+
+        /// <summary>
+        /// Send cmd for acquire a new Image from TCP
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAcqNewImage_Click(object sender, EventArgs e)
+        {
+            if (oTCPClient != null)
+            {
+                byte[] stringByte = Encoding.ASCII.GetBytes("singleSNAP");
+               
+                stream.Write(stringByte, 0, stringByte.Length);
+            }
+        }
+
+
+
+        int iTotalRead = 0;
+        bool bFoundStopCond;
+        Byte[] rawimageOriginal;
+        byte[] imageDataTemp;
+
+
+        private void ListenForInMex()
+        {
+            /*Listen for incoming messages indefinitely until connection is broke*/
+            while (oTCPClient != null && oTCPClient.Connected)
+            {
+                dataReceive = new Byte[10000];
+
+                try
+                {
+                    // receive data from stream
+                    bytesread = stream.Read(dataReceive, 0, dataReceive.Length);
+                    stringData = System.Text.Encoding.ASCII.GetString(dataReceive, 0, bytesread);
+
+                    if (stringData.Contains("sRt") || stringData.Contains("sTp"))
+                    {
+
+                        // sRt - bytes - sTp
+                        if (stringData.Contains("sRt"))
+                        {
+                            bFoundStopCond = false;
+                            rawimageOriginal = new Byte[1920 * 1080 * 3];
+
+
+                            stringData = stringData.Replace("sRt", "");
+                            bytesread = bytesread - "sRt".Length;
+
+                            iTotalRead = 0;
+
+                            if (bytesread > 0)
+                            {
+                                for (int i = 0; i < bytesread; i++)
+                                {
+                                    rawimageOriginal[i] = dataReceive[i + "sRt".Length];
+                                }
+                                iTotalRead += bytesread;
+                            }
+
+                            if (stringData.Contains("sTp"))
+                            {
+                                bFoundStopCond = true;
+                            }
+                        }
+                        if (stringData.Contains("sTp"))
+                        {
+
+                            if (!bFoundStopCond)
+                            {
+                                if (bytesread != "sTp".Length)
+                                {
+                                    for (int i = 0; i < bytesread - "sTp".Length; i++)
+                                    {
+                                        rawimageOriginal[iTotalRead + i] = dataReceive[i];
+                                    }
+                                    iTotalRead += bytesread;
+                                }
+                            }
+
+                            iTotalRead = iTotalRead - "sTp".Length;
+
+                            imageDataTemp = new byte[iTotalRead];
+
+                            for (int i = 0; i < iTotalRead; i++)
+                            {
+                                imageDataTemp[i] = rawimageOriginal[i];
+                            }
+
+                            using (MemoryStream oMemory = new MemoryStream(imageDataTemp))
+                            {
+                                Image image = Image.FromStream(oMemory);
+
+                                pictureBox1.Image = image;
+                            }                        
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (receiveThread != null && oTCPClient != null)
+            {
+                receiveThread.Abort();
+
+                // Disconnect
+                oTCPClient.Close();
+            }
+        }
+    }
+}
